eed7c4fcde96128f95ef0c7ee79ce72a0a2392c0
Nikita <nikita@it.keine.media>
2026-01-20 08:36:41 +0300

    fix docker compose command in script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
+if [ -x "$(command -v docker-compose)" ]; then
+  DOCKER_COMPOSE="docker-compose"
+elif docker compose version > /dev/null 2>&1; then
+  DOCKER_COMPOSE="docker compose"
+else
+  echo 'Error: neither docker-compose nor docker compose is installed.' >&2
   exit 1
 fi
+
+echo "Using command: $DOCKER_COMPOSE"
 
 domains=(dockr.ru www.dockr.ru)
 rsa_key_size=4096
@@ -30,7 +36,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose -f docker-compose.prod.yaml run --rm --entrypoint "\
+$DOCKER_COMPOSE -f docker-compose.prod.yaml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -39,11 +45,11 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose -f docker-compose.prod.yaml up --force-recreate -d nginx
+$DOCKER_COMPOSE -f docker-compose.prod.yaml up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate ..."
-docker-compose -f docker-compose.prod.yaml run --rm --entrypoint "\
+$DOCKER_COMPOSE -f docker-compose.prod.yaml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -65,7 +71,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose -f docker-compose.prod.yaml run --rm --entrypoint "\
+$DOCKER_COMPOSE -f docker-compose.prod.yaml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -76,4 +82,4 @@ docker-compose -f docker-compose.prod.yaml run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose -f docker-compose.prod.yaml exec nginx nginx -s reload
+$DOCKER_COMPOSE -f docker-compose.prod.yaml exec nginx nginx -s reload
